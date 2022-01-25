6fbc59c439405893c8297f85931c8e5a2ec4d5d6
Ricardo Gonçalves Ferreira <rgf.ricardo@gmail.com>
2022-01-24 23:29:04 -0300

    Added log in authentication

--- favorite_products/api/authenticate.py
+++ favorite_products/api/authenticate.py
@@ -1,22 +1,26 @@
+import logging
+
 from functools import wraps
 
 from flask_restx import abort
 
 
 def basic_authentication():
-    ...
+    logging.info('basic_authentication executado com sucesso')
+    return True
+
+
+def basic_authorization():
+    logging.info('basic_authorization executado com sucesso')
 
 
 def authenticate(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        if not getattr(func, "authenticated", True):
-            return func(*args, **kwargs)
-
-        acct = basic_authentication()  # custom account lookup function
+        acct = basic_authentication()
         if acct:
+            basic_authorization()
             return func(*args, **kwargs)
-
         abort(401)
 
     return wrapper
